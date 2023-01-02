6ca6f6e778cdd80f62482910cff778de5c1dbab1
Shayan Radi <rdishyn@gmail.com>
2023-01-02 22:12:51 +0330

    updating header

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "../src/assets/styles/public.scss";
+import "../src/assets/styles/color-pallet.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- src/layouts/header/header.layout.js
+++ src/layouts/header/header.layout.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div className="coloraa">This is header</div>;
+  return (
+    <div className="abbas">
+      <div className="coloraa">
+        <div className="flex">
+          <Link to={"#"}>شین مارکت</Link>
+          <Link to={"#"}>دسته بندی کالا ها</Link>
+          <Link to={"#"}>داغ ترین ها 🔥</Link>
+          <Link to={"#"}>اوف ترین ها 💦</Link>
+        </div>
+        <div className="flex">
+          <Link to={"#"}>🛒 سبد خرید</Link>
+          <input type="text" />
+          <div className="">
+            <p>جست و جو 🔍</p>
+          </div>
+          <div className="flex-row">
+            <Link to={"#"} className="sign-in">
+              ورود
+            </Link>
+            <Link to={"#"} className="log-in">
+              ثبت نام
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
